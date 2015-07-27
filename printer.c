0624a2595aff7148d48cb181a9c32b0afb044908
ipritish <pritishjain2009@gmail.com>
2015-07-27 21:50:15 +0530

    write with sem and fork

---
+++ printer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+
+int main (int argc, char *argv[])
+{
+    if (argc > 1)
+    {
+        printf("Passed value is %s\n",argv[1]);
+    }
+
+    return 0;
+
+}
